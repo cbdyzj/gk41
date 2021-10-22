22e9c903ef37d01e5edda274e3d926382bd2b4bf
cbdyzj <cbdyzj@jianzhao.org>
2021-10-22 23:30:40 +0800

    Add CCTV13

--- www/src/main.js
+++ www/src/main.js
@@ -2,6 +2,8 @@ import express from 'express'
 import { dirname, join } from 'path'
 import { fileURLToPath } from 'url'
 
+import tv from './tv.js'
+
 const __dirname = dirname(fileURLToPath(import.meta.url))
 
 const app = express()
@@ -10,6 +12,15 @@ app.use(express.static(join(__dirname, 'static')))
 
 app.get('/ping', (req, res) => {
     res.end('pong')
+})
+
+app.get('/tv/:channel', (req, res) => {
+    const { channel } = req.params
+    if (!channel || !tv[channel]) {
+        res.sendStatus(404)
+    } else {
+        res.redirect(tv[channel])
+    }
 })
 
 const port = process.env.PORT || 3000
