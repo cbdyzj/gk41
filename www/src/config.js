c9a75706441ae072e2cae881630c3d7c13ba52f7
cbdyzj <cbdyzj@jianzhao.org>
2021-11-13 20:03:28 +0800

    Fix PROXY

--- www/src/config.js
+++ www/src/config.js
@@ -8,4 +8,4 @@ export const rootDir = join(__dirname, '../..')
 export const publicDir = join(rootDir, 'public')
 
 export const PORT = process.env.PORT ?? 3000
-export const PROXY = process.env.PROXY ?? 'socks://127.0.0.1:9080'
+export const PROXY = process.env.PROXY ?? 'socks5://127.0.0.1:9080'
