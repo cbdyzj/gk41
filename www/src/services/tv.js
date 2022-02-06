0b3f61b8fbb73abd026470aef39bd6b5b58e78e1
cbdyzj <cbdyzj@jianzhao.org>
2022-02-06 20:04:43 +0800

    Add cctv5

--- www/src/services/tv.js
+++ www/src/services/tv.js
@@ -4,6 +4,9 @@ const channels = [{
 }, {
     name: 'cctv13',
     url: 'http://39.134.115.163:8080/PLTV/88888910/224/3221225638/index.m3u8',
+}, {
+    name: 'cctv5',
+    url: 'http://39.134.115.163:8080/PLTV/88888910/224/3221225633/index.m3u8',
 }]
 
 channels.push(...channels.map(it => {
